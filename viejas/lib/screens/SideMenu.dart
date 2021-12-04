62c9036b00c902655712b7fa892eccd0bb025327
vamsi <vamshikrishna@hyperthread.in>
2021-12-04 09:11:23 +0530

    Side menu overflow issue fixed

--- viejas/lib/screens/SideMenu.dart
+++ viejas/lib/screens/SideMenu.dart
@@ -31,46 +31,48 @@ class _SidemenuState extends State<Sidemenu> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 40, 10, 70),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              _buildSideMenuRow('Home.png', 'Home', onTap: () {
-                Navigator.of(context).pop();
-              }),
-              _buildSideMenuRow('Offers.png', 'Offers'),
-              _buildSideMenuRow('Promotions.png', 'Promotions'),
-              _buildSideMenuRow('Gaming.png', 'Gaming'),
-              _buildSideMenuRow('Hotel.png', 'Hotel'),
-              _buildSideMenuRow('Dining.png', 'Dining'),
-              _buildSideMenuRow('Music.png', 'Music and Lounges'),
-              _buildSideMenuRow('Cart.png', 'Viejas Outlet Centre'),
-              _buildSideMenuRow('Contactus.png', 'Contact us'),
-              _buildSideMenuRow('Map.png', 'Map', onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapScreen()),
-                );
-              }),
-              _buildSideMenuRow('Settings.png', 'Settings', onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Settings()),
-                );
-              }),
-              _buildSideMenuRow('Login.png', 'Login', onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                );
-              }),
-            ],
+      child: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 40, 10, 70),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _buildSideMenuRow('Home.png', 'Home', onTap: () {
+                  Navigator.of(context).pop();
+                }),
+                _buildSideMenuRow('Offers.png', 'Offers'),
+                _buildSideMenuRow('Promotions.png', 'Promotions'),
+                _buildSideMenuRow('Gaming.png', 'Gaming'),
+                _buildSideMenuRow('Hotel.png', 'Hotel'),
+                _buildSideMenuRow('Dining.png', 'Dining'),
+                _buildSideMenuRow('Music.png', 'Music and Lounges'),
+                _buildSideMenuRow('Cart.png', 'Viejas Outlet Centre'),
+                _buildSideMenuRow('Contactus.png', 'Contact us'),
+                _buildSideMenuRow('Map.png', 'Map', onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MapScreen()),
+                  );
+                }),
+                _buildSideMenuRow('Settings.png', 'Settings', onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Settings()),
+                  );
+                }),
+                _buildSideMenuRow('Login.png', 'Login', onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
